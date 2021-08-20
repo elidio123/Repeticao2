a307f3242d9d2f11364991c7b4aa14590ac05277
elidio123 <elidio.laureano@fatec.sp.gov.br>
2021-08-20 01:54:27 +0000

    Resolucao Exercicio For 2

---
+++ ExercicioRepeticao2.java
@@ -0,0 +1,10 @@
+public class ExercicioRepeticao2{
+    public static void main(String args[]){
+       int soma=0;
+       for (int i=1; i<=1000; i++){
+           soma=soma+i;          
+        }
+        System.out.println("A soma é:"+ soma);
+    }
+}
+//Imprima a soma de 1 até 1000. 
